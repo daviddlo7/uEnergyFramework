7e158281f632d9e2abd1473420dc4764f7d36cf7
David de la Osa Mostazo <david.delaosamostazo@telefonica.com>
2025-03-28 12:18:03 +0100

    Update grpc_to_pods.sh

--- grpc_to_pods.sh
+++ grpc_to_pods.sh
@@ -1,6 +1,9 @@
-grpcurl -plaintext 10.152.183.13:50051 list
+grpcurl -plaintext 10.152.183.11:50051 list
 grpcurl -plaintext 10.152.183.12:50051 list
-grpcurl -plaintext 10.152.183.13:50051 describe analytics.Analytics
+grpcurl -plaintext 10.152.183.13:50051 list
+grpcurl -plaintext 10.152.183.11:50051 describe webui.WebUi
 grpcurl -plaintext 10.152.183.12:50051 describe energycollector.EnergyCollector
-grpcurl -plaintext -d '{"name": "example_name"}' 10.152.183.13:50051 analytics.Analytics/RunAnalytics
+grpcurl -plaintext 10.152.183.13:50051 describe analytics.Analytics
+grpcurl -plaintext -d '{"name": "example_name"}' 10.152.183.11:50051 webui.WebUi/WebUi
 grpcurl -plaintext -d '{"device": "example_device"}' 10.152.183.12:50051 energycollector.EnergyCollector/RunTest
+grpcurl -plaintext -d '{"name": "example_name"}' 10.152.183.13:50051 analytics.Analytics/RunAnalytics
